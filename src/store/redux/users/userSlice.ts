b44b554ef1c921b24294865adccdbe141332bf1e
AGaleea <a_galeeva@hotmail.com>
2023-12-19 16:03:17 +0100

    lesson33

--- src/store/redux/users/userSlice.ts
+++ src/store/redux/users/userSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import {UsersState, User} from './types'
+import {v4} from 'uuid';
+
+import {UsersState, User, AddUserPayload} from './types'
 
 const usersInitialState: UsersState = {
   users: [],
@@ -9,9 +11,20 @@ const usersSlice = createSlice({
   name: 'USERS',
   initialState: usersInitialState,
   reducers: {
-    addUser: (state: UsersState, { payload }: PayloadAction<User>) => {
-      state.users = [...state.users, payload]
-    }
+    // 2 аргумент у любого reducer - это обьект action
+    // что такое обьект action?
+    // action = {
+    //  payload: {} //это данные которые мы передаем из компонента при диспатче экшена в круглых скобках
+    // type: "USER/addUser" // это автоматичекси сгенерированный тип, мы можем увидить его в redux Devtools
+    // }
+    addUser: (state: UsersState, { payload }: PayloadAction<AddUserPayload>) => {
+      // каждому новому добавленному юзеру генерируется уникальный id через библиотеку "uuid"
+      state.users = [...state.users, {...payload, id: v4()}]
+    },
+    deleteUser: (state: UsersState, { payload }: PayloadAction<string>) => {
+      state.users = [...state.users].filter((userObject) => userObject.id !== payload)
+    },
+    deleteAllUsers: () => usersInitialState
   }
 })
 
